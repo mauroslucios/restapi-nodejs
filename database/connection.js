19fb24ba4acf186b8ef787706e6706977dee2e16
mauroslucios <mauroslucios@gmail.com>
2022-02-24 13:23:42 -0300

    Modificado acesso ao banco mongodb

--- database/connection.js
+++ database/connection.js
@@ -7,13 +7,3 @@ mongoose.connect('mongodb://172.21.0.2:27017/apirest').then(()=>{
 
 mongoose.Promise = global.Promise
 module.exports = mongoose
-
-
-/*const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://mauroslucios:P@ssWor4@cluster0.zspdu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-    .then(() =>{
-        console.log('Conectou ao banco!')
-    })
-    .catch((err) => console.log(err));
-
-*/
