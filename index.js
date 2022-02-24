a8cbc7b49736bd743b573d378a0452c0b9106803
mauroslucios <mauroslucios@gmail.com>
2022-02-24 18:50:04 -0300

    Atualizando index.js

--- index.js
+++ index.js
@@ -2,16 +2,51 @@ const express = require('express');
 const app = express();
 const path = require("path")
 const db = require("./database/connection")
+const bodyParser = require("body-parser")
+//const admin = require("./routes/admin")
+require('./models/Departamento');
+const Departamento = db.model("departamentos");
+const router = express.Router();
+//const bodyParser = require("body-parser")
 
-const PORT = 3000;
+    //Public static
+    app.use(express.static(path.join(__dirname,"/public")))
+
+     //Body Parser
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
+    //rotas admin
+    //app.use('/admin', admin)
 
     app.get("/", (req,res)=>{
         res.json({ message: "Hello API"});
     })
 
-    //Public static
-    app.use(express.static(path.join(__dirname,"/public")))
+    app.post("/departamento/novo", async (req,res)=>{
+        const novoDepartamento = await {
+            nome: req.body.nome
+        }
+        new Departamento(novoDepartamento).save().then(()=>{
+            res.status(201).json({message: 'Departamento criado com sucesso!'});
+        }).catch((error) =>{
+            res.status(500).json({erro : error });
+        })
+    })
 
+    app.get("/departamentos", async (req,res)=>{
+        try{
+            const people = await Departamento.find();
+            res.status(200).json(people);
+        }catch(error){
+            res.status(500).json({erro: error})
+        }
+        
+    })
+  
+    
+
+const PORT = 3000;
 app.listen(PORT,()=>{
     console.log(`app running on url http://localhost:${PORT}`);
 })
