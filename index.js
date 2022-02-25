9a0856bc3c42787c2c9ef0ad3416ece367a8e838
mauroslucios <mauroslucios@gmail.com>
2022-02-25 19:00:01 -0300

    Adicionando rotas admin usando router

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
-const path = require("path")
-const db = require("./database/connection")
-const bodyParser = require("body-parser")
-//const admin = require("./routes/admin")
-require('./models/Departamento');
-const Departamento = db.model("departamentos");
-const router = express.Router();
-//const bodyParser = require("body-parser")
+const path = require('path');
+const db = require('./database/connection');
+const bodyParser = require('body-parser');
+const admin = require('./routes/admin');
+const swaggerUi = require('swagger-ui-express');
+const mainRoutes = require('./routes/main');
+
+
 
     //Public static
     app.use(express.static(path.join(__dirname,"/public")))
@@ -17,69 +17,13 @@ const router = express.Router();
     app.use(bodyParser.json())
 
     //rotas admin
-    //app.use('/admin', admin)
+    app.use('/admin', admin)
 
     app.get("/", (req,res)=>{
         res.json({ message: "Hello API"});
     })
 
-    app.post("/departamento/novo", async (req,res)=>{
-        const novoDepartamento = await {
-            nome: req.body.nome
-        }
-        new Departamento(novoDepartamento).save().then(()=>{
-            res.status(201).json({message: 'Departamento criado com sucesso!'});
-        }).catch((error) =>{
-            res.status(500).json({erro : error });
-        })
-    })
-
-    app.get("/departamentos", async (req,res)=>{
-        try{
-            const people = await Departamento.find();
-            res.status(200).json(people);
-        }catch(error){
-            res.status(500).json({erro: error})
-        }
-        
-    })
-
-    app.get("/departamentos/:id", async(req, res)=>{
-        const id = req.params.id;
-        try{
-            const departamento = await Departamento.findOne({_id: id});
-            if(!departamento){
-                res.status(422).json({message: 'Departamento não encontado verifique o ID!'})
-                return
-            }
-            res.status(200).json(departamento);
-        }catch(error){
-            res.status(500).json({ erro: error});
-        }
-    })
-
-    app.patch("/departamento/:id", async(req,res)=>{
-        const id = req.params.id;
-        const { nome, dataCadastro } = req.body;
-        const departamento = {
-            nome, dataCadastro
-        }
-
-        try{
-            const updateDepartamento = await Departamento.updateOne({ _id: id}, departamento);
-            if(updateDepartamento.matchedCount === 0){
-                res.status(422).json({message: "Departamento não encontrado!"});
-                return
-            }
-            res.status(200).json(departamento);
-        }catch(error){
-            res.status(500).json({erro: error });
-        }
-    })
-
-
-
-
+   
     
 
 const PORT = 3000;
---
+++ routes/admin.js
@@ -0,0 +1,62 @@
+const express = require('express')
+require('../models/Departamento');
+const db = require("../database/connection")
+const Departamento = db.model("departamentos");
+const router = express.Router();
+
+router.post("/departamento/novo", async (req,res)=>{
+    const novoDepartamento = await {
+        nome: req.body.nome
+    }
+    new Departamento(novoDepartamento).save().then(()=>{
+        res.status(201).json({message: 'Departamento criado com sucesso!'});
+    }).catch((error) =>{
+        res.status(500).json({erro : error });
+    })
+})
+
+router.get("/departamentos", async (req,res)=>{
+    try{
+        const people = await Departamento.find();
+        res.status(200).json(people);
+    }catch(error){
+        res.status(500).json({erro: error})
+    }
+    
+})
+
+router.get("/departamentos/:id", async(req, res)=>{
+    const id = req.params.id;
+    try{
+        const departamento = await Departamento.findOne({_id: id});
+        if(!departamento){
+            res.status(422).json({message: 'Departamento não encontado verifique o ID!'})
+            return
+        }
+        res.status(200).json(departamento);
+    }catch(error){
+        res.status(500).json({ erro: error});
+    }
+})
+
+router.patch("/departamento/:id", async(req,res)=>{
+    const id = req.params.id;
+    const { nome, dataCadastro } = req.body;
+    const departamento = {
+        nome, dataCadastro
+    }
+
+    try{
+        const updateDepartamento = await Departamento.updateOne({ _id: id}, departamento);
+        if(updateDepartamento.matchedCount === 0){
+            res.status(422).json({message: "Departamento não encontrado!"});
+            return
+        }
+        res.status(200).json(departamento);
+    }catch(error){
+        res.status(500).json({erro: error });
+    }
+})
+
+
+module.exports = router
