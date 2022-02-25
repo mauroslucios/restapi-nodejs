02dd3f65074443dc5e28be201d32a3860de3bdd8
mauroslucios <mauroslucios@gmail.com>
2022-02-25 19:43:12 -0300

    Adicionado configurações e rota para a documentação swagger

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const db = require('./database/connection');
 const bodyParser = require('body-parser');
 const admin = require('./routes/admin');
 const swaggerUi = require('swagger-ui-express');
-const mainRoutes = require('./routes/main');
+const swaggerFile = require('./swagger/swagger_output.json');
 
 
+    //swagger
+    app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+    app.use(express.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
 
     //Public static
     app.use(express.static(path.join(__dirname,"/public")))
@@ -17,8 +21,8 @@ const mainRoutes = require('./routes/main');
     app.use(bodyParser.json())
 
     //rotas admin
-    app.use('/admin', admin)
-
+    app.use('/admin', admin);
+    
     app.get("/", (req,res)=>{
         res.json({ message: "Hello API"});
     })
